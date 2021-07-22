9cfd8ad8616e021e1823189fb7526f4766f6331c
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-07-22 21:21:36 +0530

    Add initial index page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,3 +1,53 @@
-export default function Home() {
-    return <div>Home</div>;
+import fs from "fs";
+
+interface HomeProps {
+    main: string;
+    information: string;
+    motivation: string;
+    coreValues: [string]
 };
+
+export default function Home({ main, information, motivation, coreValues }: HomeProps) {
+    return (
+        <div>
+            <main>
+                <h1>The Notes Application</h1>
+                <p>{main}</p>
+            </main>
+            <section>
+                <h2>Information</h2>
+                <p>{information}</p>
+            </section>
+            <section>
+                <h2>Motivation</h2>
+                <p>{motivation}</p>
+            </section>
+            <section>
+                <h2>Core Values</h2>
+                <ul>
+                    {coreValues.map((coreValue, index) => 
+                        <li key={index}>{coreValue}</li>
+                    )}
+                </ul>
+            </section>
+        </div>
+    );
+};
+
+export async function getStaticProps() {
+    const main = fs.readFileSync("content/main.text").toString();
+    const information = fs.readFileSync("content/information.text").toString();
+    const motivation = fs.readFileSync("content/motivation.text").toString();
+    const coreValues = JSON.parse(
+        fs.readFileSync("content/core-values.json").toString()
+    );
+
+    return {
+        props: {
+            main,
+            information,
+            motivation,
+            coreValues
+        }
+    }
+}
